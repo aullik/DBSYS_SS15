9788e7769d14db122bc0e9f35b4f8cdc21ad221c
Nicolas Wehrle <niwehrle@htwg-konstanz.de>
2015-06-29 17:34:02 +0200

    corrected imports

--- DBSYS_SS15/src/de/dbsys/view/root/Aside/loginLayout/LoginLayoutController.java
+++ DBSYS_SS15/src/de/dbsys/view/root/Aside/loginLayout/LoginLayoutController.java
@@ -3,31 +3,35 @@ package de.dbsys.view.root.Aside.loginLayout;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 
 public class LoginLayoutController implements Initializable {
 
-	@FXML
-	private TextField emailTF;
+   @FXML
+   private TextField emailTF;
 
-	@FXML
-    private PasswordField passwortTF;
+   @FXML
+   private PasswordField passwortTF;
 
-	@Override
+   @Override
    public void initialize(final URL location, final ResourceBundle resources) {
       // TODO Auto-generated method stub
-	  
+
    }
-	
-	@FXML
-	void handleLogin(ActionEvent event) {
 
-	}
+   @FXML
+      void handleLogin(final ActionEvent event) {
 
-	@FXML
-	void handleRegisterNew(ActionEvent event) {
+   }
 
-	}
+   @FXML
+      void handleRegisterNew(final ActionEvent event) {
+
+   }
 
 }
