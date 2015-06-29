bdd8bd0cc82b665d17936ecfc0d6fa93aeff5c27
Nicolas Wehrle <niwehrle@htwg-konstanz.de>
2015-06-29 17:13:41 +0200

    changed names

--- DBSYS_SS15/src/de/dbsys/view/root/Aside/mainLayout/BookingConfirmationLoader.java
+++
@@ -1,9 +0,0 @@
-package de.dbsys.view.root.Aside.mainLayout;
-
-import de.dbsys.view.MVCLoader;
-
-public class BookingConfirmationLoader extends MVCLoader{
-	public BookingConfirmationLoader() {
-		super(() -> new BookingConfirmationController());
-	}
-}
